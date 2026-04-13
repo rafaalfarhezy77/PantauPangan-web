37309746be204b0ed45c352218bbeabbbc118519
rafaalfarhezy77 <rafaalfarhezy@student.uns.ac.id>
2026-04-13 22:21:05 +0700

    Fix : Minor Fixing

--- Proses/prosesLogin.php
+++ Proses/prosesLogin.php
@@ -22,8 +22,6 @@
             $_SESSION['role'] = $user['role'];
             $_SESSION['username'] = $user['username'];
 
-            $_SESSION['username'] = $row['username'];
-
             echo json_encode([
                 "success" => true,
                 "username" => $user["username"],
